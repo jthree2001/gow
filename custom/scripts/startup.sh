a6c7402359b1cd8ef0f045f94cb9715ee934a6e9
jthree2001 <lawsmichael@gmail.com>
2023-01-20 09:14:54 -0500

    feat working steam image and updating requirements

--- custom/scripts/startup.sh
+++ custom/scripts/startup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+
 set -e
 
 source /opt/gow/bash-lib/utils.sh
 
-gow_log "Starting Steam with DISPLAY=${DISPLAY}"
+gow_log "Waiting for X Server $DISPLAY to be available"
+/opt/gow/wait-x11
 
-# Recursively creating Steam necessary folders (https://github.com/ValveSoftware/steam-for-linux/issues/6492)
-mkdir -p "$HOME/.steam/ubuntu12_32/steam-runtime"
-
-# Start Steam. Use `sudo` to make sure that group membership gets reloaded
-exec /usr/games/steam
+# Launch the container's startup script
+exec /opt/gow/startup-app.sh
