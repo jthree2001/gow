bf1e87fdd461c5149cec8d04e71a1192ee5a48d0
jthree2001 <lawsmichael@gmail.com>
2023-02-24 09:50:56 -0500

    feat(steam): got the arch steam version up and working

--- custom/scripts/startup.sh
+++ custom/scripts/startup.sh
@@ -4,8 +4,5 @@ set -e
 
 source /opt/gow/bash-lib/utils.sh
 
-gow_log "Waiting for X Server $DISPLAY to be available"
-/opt/gow/wait-x11
-
 # Launch the container's startup script
 exec /opt/gow/startup-app.sh
